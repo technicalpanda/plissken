65fa33d745b318359593fbc2d09540d2874da5fe
MichaelAChrisco <michaelachrisco@gmail.com>
2014-12-31 10:12:15 -0800

    Rubocop now passes.

--- lib/plissken/ext/hash/to_snake_keys.rb
+++ lib/plissken/ext/hash/to_snake_keys.rb
@@ -1,3 +1,4 @@
+# Hash.to_snake_keys
 class Hash
   # Recursively converts CamelCase and camelBack JSON-style hash keys to
   # Rubyish snake_case, suitable for use during instantiation of Ruby
@@ -5,12 +6,12 @@ class Hash
   #
   def to_snake_keys(value = self)
     case value
-      when Array
-        value.map { |v| to_snake_keys(v) }
-      when Hash
-        Hash[value.map { |k, v| [underscore_key(k).to_sym, to_snake_keys(v)] }]
-      else
-        value
+    when Array
+      value.map { |v| to_snake_keys(v) }
+    when Hash
+      Hash[value.map { |k, v| [underscore_key(k).to_sym, to_snake_keys(v)] }]
+    else
+      value
     end
   end
 
@@ -27,11 +28,10 @@ class Hash
   end
 
   def underscore(string)
-    string.gsub(/::/, '/').
-    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-    gsub(/([a-z\d])([A-Z])/,'\1_\2').
-    tr("-", "_").
-    downcase
+    string.gsub(/::/, '/')
+      .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+      .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+      .tr('-', '_')
+      .downcase
   end
 end
-  
