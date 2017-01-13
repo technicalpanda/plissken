b404603cb50443d34052bcd2ba477473ac78ad07
shotat <shotat@users.noreply.github.com>
2017-01-13 13:26:38 +0900

    memoize

--- lib/plissken/ext/hash/to_snake_keys.rb
+++ lib/plissken/ext/hash/to_snake_keys.rb
@@ -32,10 +32,14 @@ class Hash
   end
 
   def underscore(string)
-    string.gsub(/::/, '/')
-      .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-      .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-      .tr('-', '_')
-      .downcase
+    @__memoize_underscore ||= {}
+    return @__memoize_underscore[string] if @__memoize_underscore[string]
+    @__memoize_underscore[string] =
+      string.gsub(/::/, '/')
+            .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+            .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+            .tr('-', '_')
+            .downcase
+    @__memoize_underscore[string]
   end
 end
